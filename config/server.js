88dc00ee1e2d949c7367f37eff694a9cf11a7d2b
xy hua <1095159835@qq.com>
2021-07-11 21:53:14 +0800

    firtst commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cf843706014a89322d27451d0256e659'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8c398769-6b3d-4eb9-9c67-a7ad402b633c'
+};
